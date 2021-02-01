4ba8173ec7ef202b7b08157ab9538a20401d1b6f
racross1 <racross1@gmail.com>
2021-01-31 16:51:17 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(src, cb) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+      r.push(cb(src[i])) 
+    }
+    return r
+  }
+
+  function reduce(src, cb, start=0) {
+    let sum = (!!start) ? start : src[0]
+    let i = (!!start) ? 0 : 1
+    for (; i < src.length; i++) {
+      sum = (cb(sum, src[i])) 
+    }
+    return sum
+  }
